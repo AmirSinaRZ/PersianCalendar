2e973b1308582399fb7b282159d547c792c0c678
AmirSinaRZ <amirsinarazghandy@gmail.com>
2026-07-10 03:13:56 +0330

    updated to CalendarDay

--- persiancalendar/src/main/java/com/amirsinarz/persiancalendar/model/CalendarDay.kt
+++ persiancalendar/src/main/java/com/amirsinarz/persiancalendar/model/CalendarDay.kt
@@ -1,11 +1,16 @@
 package com.amirsinarz.persiancalendar.model
 
 import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.chrono.HijrahDate
 
-data class JalaliDay(
+data class CalendarDay(
     val dayOfWeek: DayOfWeek,
     val isDayOff: Boolean,
     val dayOfMonth: Int,
+    val jalaliDate: JalaliDate,
+    val gregorianDate: LocalDate,
+    val hijrahDate: HijrahDate,
     val isToday: Boolean
 )
 //TODO: add desc and events
